b7ff439a1731e91f72157c4f2a36e1d9c20dcc26
Evgeniy Budaev <budaev.e@gmail.com>
2023-06-03 20:15:46 +0300

    Squares of a Sorted Array

---
+++ arrays101/intro/squaresOfASortedArray.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const compareNumbers = (a, b) => {
+    return a - b;
+};
+
+const sortedSquares = function(nums) {
+    const numsPositive = nums.map(item => item < 0 ? item * -1 : item);
+    const numsSorted = numsPositive.sort(compareNumbers);
+
+    return numsSorted.map(item => item * item);
+};
+
+console.log(sortedSquares([-4,-1,0,3,10])); // [0,1,9,16,100]
+console.log(sortedSquares([-7,-3,2,3,11])); // [4,9,9,49,121]
