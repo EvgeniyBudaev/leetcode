d6c9d8de4442a3e72b9171ae72b67b06a54ff425
Evgeniy Budaev <budaev.e@gmail.com>
2023-06-03 14:41:46 +0300

    Max Consecutive Ones

---
+++ arrays101/intro/maxConsecutiveOnes.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findMaxConsecutiveOnes = function(nums) {
+    let max = 0;
+    let currentCountOfOnes = 0;
+
+    nums.forEach(num => {
+        if (num === 1) {
+            currentCountOfOnes += 1;
+            max = Math.max(max, currentCountOfOnes);
+        } else {
+            currentCountOfOnes = 0;
+        }
+    });
+
+    return max;
+};
+
+console.log(findMaxConsecutiveOnes([1,1,0,1,1,1])); // 3
+console.log(findMaxConsecutiveOnes([1,0,1,1,0,1])); // 2
