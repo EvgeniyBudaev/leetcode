2482db42fc79021ab79976dc0abffeb07635a441
Evgeniy Budaev <budaev.e@gmail.com>
2023-06-09 07:47:11 +0300

    Find All Numbers Disappeared in an Array

---
+++ arrays101/conclusion/findAllNumbersDisappearedInAnArray.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function(nums) {
+    const arrayLength = nums.length;
+
+    // Маркируем все числа, которые появились в массиве
+    for (let i = 0; i < arrayLength; i++) {
+        const num = Math.abs(nums[i]);
+        nums[num - 1] = -Math.abs(nums[num - 1]);
+    }
+
+    // Добавляем в массив все числа, которые не были помечены
+    for (let i = 0; i < arrayLength; i++) {
+        if (nums[i] > 0) {
+            nums.push(i + 1);
+        }
+    }
+    // Удаляем помеченные числа из массива
+    nums.splice(0, arrayLength);
+    return nums;
+};
+
+console.log(findDisappearedNumbers([4,3,2,7,8,2,3,1])); // [5,6]
+console.log(findDisappearedNumbers([1,1])); // [2]
