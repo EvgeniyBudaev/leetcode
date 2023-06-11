8ea0ba069c9189521d9c19eb1602391905b8913f
Evgeniy Budaev <budaev.e@gmail.com>
2023-06-11 09:51:16 +0300

    Метод двух указателей. Two sum

---
+++ dataStructuresAndAlgorithms/arraysAndStrings/checkForTarget.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+var checkForTarget = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let curr = nums[left] + nums[right];
+        if (curr === target) {
+            return true;
+        }
+
+        if (curr > target) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+
+    return false;
+}
+
+console.log(checkForTarget([1, 2, 4, 6, 6, 8, 9, 14, 15], 13)); // true
