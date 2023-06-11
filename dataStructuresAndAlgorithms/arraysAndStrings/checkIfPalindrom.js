dab9c68cb8f3908e027e8df2a89a6d6610002ecd
Evgeniy Budaev <budaev.e@gmail.com>
2023-06-11 09:39:02 +0300

    Метод двух указателей. Палиндром

---
+++ dataStructuresAndAlgorithms/arraysAndStrings/checkIfPalindrom.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkIfPalindrome = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+console.log(checkIfPalindrome(abcdcba)); // true
