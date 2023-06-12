5bdfe33952a9392435307584784a8e1b072a9b7c
Evgeniy Budaev <budaev.e@gmail.com>
2023-06-12 13:07:57 +0300

    Скользящее окно. Фиксированный размер окна

--- dataStructuresAndAlgorithms/arraysAndStrings/sliding window/findBestSubarray.js
+++ dataStructuresAndAlgorithms/arraysAndStrings/sliding window/findBestSubarray.js
@@ -5,20 +5,22 @@
  */
 
 // Дан массив целых чисел nums и целое число k, найдите сумму подмассива с наибольшей суммой, длина которой равна k.
-var findBestSubarray = function(nums, k) {
-    let curr = 0; // некоторые данные для отслеживания окна
+var findBestSubarray = function (nums, k) {
+  let curr = 0; // некоторые данные для отслеживания окна
 
-    // построить первое окно
-    for (let i = 0; i < k; i++) {
-        curr += nums[i];
-    }
+  // построить первое окно
+  for (let i = 0; i < k; i++) {
+    curr += nums[i];
+  }
 
-    let ans = curr; // переменная ответа , вероятно, равно curr здесь в зависимости от проблемы
+  let ans = curr; // переменная ответа , вероятно, равно curr здесь в зависимости от проблемы
 
-    for (let i = k; i < nums.length; i++) {
-        curr += nums[i] - nums[i - k];
-        ans = Math.max(ans, curr);
-    }
+  for (let i = k; i < nums.length; i++) {
+    curr += nums[i] - nums[i - k];
+    ans = Math.max(ans, curr);
+  }
 
-    return ans;
-}
+  return ans;
+};
+
+console.log(findBestSubarray([0, 1, 2], 2)); // 3
