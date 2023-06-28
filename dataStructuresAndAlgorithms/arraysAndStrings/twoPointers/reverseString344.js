c25f592717b1811dc81ff90c12ae990c3dc9e67f
Evgeniy Budaev <budaev.e@gmail.com>
2023-06-28 16:57:19 +0300

    Reverse String

--- dataStructuresAndAlgorithms/arraysAndStrings/twoPointers/reverseString344.js
+++ dataStructuresAndAlgorithms/arraysAndStrings/twoPointers/reverseString344.js
@@ -2,6 +2,10 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
+
+/** Напишите функцию, которая переворачивает строку. Входная строка задается как массив символов s.
+
+Вы должны сделать это, изменив входной массив на месте с O(1) дополнительной памятью **/
 var reverseString = function(s) {
     let left = 0;
     let right = s.length - 1;
